d7959f762bb836191787b8973b1b745750278bc2
PD-Mera <phuongdong1772000@gmail.com>
2023-07-13 07:20:43 +0000

    update stable version 0.0.7

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='nvidia-smi2',
-    version='0.0.2',
+    version='0.0.7',
     install_requires=[
         'termcolor',
     ],
     scripts=[
         'bin/nvidia-smi2'
     ],
-    packages=find_packages(
-        # All keyword arguments below are optional:
-        where='nvidia-smi2',  # '.' by default
-    )
+    packages=find_packages(),
+    include_package_data=True
 )
